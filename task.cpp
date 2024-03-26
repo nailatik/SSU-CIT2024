371a48dd4d10c6e9439eab88c770a4c68dd198cd
Niall Tugushev <tugushev_06@mail.ru>
2024-03-26 21:14:46 +0300

    Update 1.1: This version of code can counting Fibonacci numbers. Just enter N and cheak result.

--- task.cpp
+++ task.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+int fib(int n){
+	if(n == 0 || n == 1){
+		return n;
+	}
+	return fib(n-1) + fib(n-2);
+}
+
 int main(){
-	std::cout << "Hello, World!\n";
+	int n;
+	std::cout << "Enter N: ";
+	std::cin >> n;
+	std::cout << "Hello, World! People, look at your fibbonachi " << "(n = " << n << "):" << '\t' << fib(n) << '\n';
 
 	return 0;
 }	
