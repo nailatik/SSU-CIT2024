72ed126aebee3c33f33ff16c29fb346ea23abfa9
Niall Tugushev <nail.tugushev30@yandex.ru>
2024-03-27 08:01:34 +0300

    Update 1.2: The basic Fibonacci number search function has been modified. The sequence starts from 0. More details - optimal exception checking for N and changes when calling a function.

--- task.cpp
+++ task.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int fib(int n){
-	if(n == 0 || n == 1){
+	if(n <= 1){
 		return n;
 	}
 	return fib(n-1) + fib(n-2);
@@ -11,7 +11,7 @@ int main(){
 	int n;
 	std::cout << "Enter N: ";
 	std::cin >> n;
-	std::cout << "Hello, World! People, look at your fibbonachi " << "(n = " << n << "):" << '\t' << fib(n) << '\n';
+	std::cout << "Hello, World! People, look at your fibbonachi " << "(n = " << n << "):" << '\t' << fib(n-1) << '\n';
 
 	return 0;
 }	
